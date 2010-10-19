13ba7067c47ea081393e7579621a6ed03aba2ca0
pieper <pieper@3bd1e089-480b-0410-8dfb-8563597acbee>
2010-10-19 19:53:33 +0000

    ENH: merge from trunk: improved background error handling
    git-svn-id: http://svn.slicer.org/Slicer3/branches/Slicer-3-6@15257 3bd1e089-480b-0410-8dfb-8563597acbee

--- Base/GUI/Tcl/SliceViewerInteractor.tcl
+++ Base/GUI/Tcl/SliceViewerInteractor.tcl
@@ -41,10 +41,44 @@ proc SliceViewerShutdown { sliceGUI } {
   }
 }
 
+proc __Slicer_bgerror {m} { 
+  $::slicer3::Application ErrorMessage "$m"
+  $::slicer3::Application ErrorMessage $::errorInfo
+  if { [string match *alloc* $m] } {
+    set submessage "You may have run out of memory.  Please save your work."
+  } else {
+    set submessage "This may be a harmless warning, but please use caution."
+  }
+  if { !$::slicer3::TEST_MODE } {
+    set msg "Slicer has encountered an unhandled background error:\n\n  $m\n\n$submessage\nSee the Error Log for more information."
+    set dialog [vtkKWMessageDialog New]
+    $dialog SetParent [$::slicer3::ApplicationGUI GetMainSlicerWindow]
+    $dialog SetMasterWindow [$::slicer3::ApplicationGUI GetMainSlicerWindow]
+    $dialog SetStyleToMessage
+    $dialog SetText $msg
+    $dialog Create
+    $dialog Invoke
+    $dialog Delete
+  }
+}
+
+
 proc SliceViewerHandleEvent {sliceGUI event} {
   # initialize on first call 
+  # -- set up our custom bgerror command (it doesn't seem possible to do this
+  #    during the Slicer3.cxx startup, so we do it here if needed)
   # -- allows clean shutdown because each vtkSlicerSliceGUI can shutdown in destructor
   # -- this is very light weight, so no problem calling every time
+
+  # hack: peek at the string version of the proc to see if kwwidgets
+  # has replaced the system version of bgerror yet - once it has done
+  # so, we want to take over with our own version
+  if { [info command ::__bgerror_kwwidgets] == "" } {
+    if { [info command ::bgerror] != "" && [string match " global Application*" [info body ::bgerror]] } {
+      rename ::bgerror ::__bgerror_kwwidgets
+      rename ::__Slicer_bgerror ::bgerror
+    }
+  }
 
   SliceViewerInitialize $sliceGUI
 }
