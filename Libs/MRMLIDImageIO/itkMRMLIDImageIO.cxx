4239713313f13698c3c67a9b9bad45fc0ff04c85
francois_budin <francois_budin@3bd1e089-480b-0410-8dfb-8563597acbee>
2010-11-24 15:18:39 +0000

    BUG:Division by 0 for gradient 0 of DWI image
    git-svn-id: http://svn.slicer.org/Slicer3/branches/Slicer-3-6@15573 3bd1e089-480b-0410-8dfb-8563597acbee

--- Libs/MRMLIDImageIO/itkMRMLIDImageIO.cxx
+++ Libs/MRMLIDImageIO/itkMRMLIDImageIO.cxx
@@ -814,12 +814,21 @@ MRMLIDImageIO
         }
       sum = sqrt(sum);
       bvalues.push_back( sum * maxBValue );
-
-      for (unsigned int i=0; i < 3; ++i)
+      /*Checks that the sum is not 0 to avoid dividing by 0
+      The value 1.0e-12 is used in
+      ../Slicer3-lib/Insight/Code/BasicFilters/itkImplicitManifoldNormalVectorFilter.h
+       line 58:
+         * The MinVectorNorm parameter determines the minumum vector norm allowed
+         * (to avoid divide by 0). The default for MinVectorNorm is 10^-6 which is designed
+         * to work with an input image of floats. This value can be lowered to 10^-12
+         * if the input image is doubles.*/
+      if( sum > 1.0e-12 )
         {
-        gradient[i] /= sum;
+        for (unsigned int i=0; i < 3; ++i)
+          {
+          gradient[i] /= sum;
+          }
         }
-
       gradients.push_back(gradient);
       }
     }
