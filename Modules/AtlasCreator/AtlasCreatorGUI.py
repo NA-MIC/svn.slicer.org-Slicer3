689edf56cb1af5848d13b144247877afe2eaf15a
haehn <haehn@3bd1e089-480b-0410-8dfb-8563597acbee>
2011-01-20 20:56:38 +0000

    ENH: Atlas Creator: Merge from trunk and enable in CMakeLists by default.
    git-svn-id: http://svn.slicer.org/Slicer3/branches/Slicer-3-6@15793 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Modules/AtlasCreator/AtlasCreatorGUI.py
@@ -0,0 +1,286 @@
+from SlicerScriptedModule import ScriptedModuleGUI
+from Slicer import slicer
+from operator import itemgetter, attrgetter
+import glob
+import os
+
+from AtlasCreatorHelper import AtlasCreatorHelper
+from AtlasCreatorLogic import AtlasCreatorLogic
+
+vtkKWPushButton_InvokedEvent = 10000
+
+vtkKWFileBrowserDialog_FileNameChangedEvent = 15000
+
+vtkMRMLScene_CloseEvent = 66003
+
+vtkSlicerNodeSelectorWidget_NodeSelectedEvent = 11000
+
+class AtlasCreatorGUI(ScriptedModuleGUI):
+
+    def __init__(self):
+
+        ScriptedModuleGUI.__init__(self)
+
+        self.SetCategory("Registration")
+        self.SetGUIName("AtlasCreator")
+
+        self._moduleFrame = slicer.vtkSlicerModuleCollapsibleFrame()
+        self._topFrame = slicer.vtkSlicerModuleCollapsibleFrame()
+
+        self._origDirButton = slicer.vtkKWLoadSaveButtonWithLabel()
+        self._segDirButton = slicer.vtkKWLoadSaveButtonWithLabel()
+        self._outDirButton = slicer.vtkKWLoadSaveButtonWithLabel()
+
+        self._secondFrame = slicer.vtkSlicerModuleCollapsibleFrame()
+
+        self._defaultCaseEntry = slicer.vtkKWComboBoxWithLabel()
+
+        self._regTypeRadios = slicer.vtkKWRadioButtonSetWithLabel()
+
+        self._saveTransformCheckBox = slicer.vtkKWCheckButtonWithLabel()
+        self._saveDeformationFieldCheckBox = slicer.vtkKWCheckButtonWithLabel()
+
+        self._generateButton = slicer.vtkKWPushButton()
+
+        self._helper = AtlasCreatorHelper(self)
+
+        self._logic = AtlasCreatorLogic(self)
+
+        self._updating = 0
+
+    def Destructor(self):
+
+
+
+        self._helper = None
+        self._logic = None
+
+    def RemoveMRMLNodeObservers(self):
+        pass
+    
+    def RemoveLogicObservers(self):
+        pass
+
+    def AddGUIObservers(self):
+        self._generateButtonTag = self.AddObserverByNumber(self._generateButton,vtkKWPushButton_InvokedEvent)
+        self._origDirButtonTag = self.AddObserverByNumber(self._origDirButton.GetWidget().GetLoadSaveDialog(),vtkKWFileBrowserDialog_FileNameChangedEvent)
+        self._segDirButtonTag = self.AddObserverByNumber(self._segDirButton.GetWidget().GetLoadSaveDialog(),vtkKWFileBrowserDialog_FileNameChangedEvent)
+
+    def RemoveGUIObservers(self):
+        pass
+
+    def ProcessGUIEvents(self,caller,event):
+        if not self._updating:
+
+            if caller == self._generateButton and event == vtkKWPushButton_InvokedEvent:
+                self.GenerateAtlas()
+            elif caller == self._origDirButton.GetWidget().GetLoadSaveDialog() and event == vtkKWFileBrowserDialog_FileNameChangedEvent:
+                self.UpdateCaseCombobox()
+            elif caller == self._segDirButton.GetWidget().GetLoadSaveDialog() and event == vtkKWFileBrowserDialog_FileNameChangedEvent:
+                self.UpdateCaseCombobox()
+
+
+    def UpdateCaseCombobox(self):
+        if self._origDirButton.GetWidget().GetFileName() and self._segDirButton.GetWidget().GetFileName():
+            # originals and segmentations dir were configured, now we parse for potential image data
+            nrrdFiles = glob.glob(os.path.join(self._origDirButton.GetWidget().GetFileName(), '*.nrrd'))
+            nhdrFiles = glob.glob(os.path.join(self._origDirButton.GetWidget().GetFileName(), '*.nhdr'))
+            hdrFiles = glob.glob(os.path.join(self._origDirButton.GetWidget().GetFileName(), '*.hdr'))          
+            mhdFiles = glob.glob(os.path.join(self._origDirButton.GetWidget().GetFileName(), '*.mhd'))          
+            mhaFiles = glob.glob(os.path.join(self._origDirButton.GetWidget().GetFileName(), '*.mha'))
+        
+            listOfFiles = [
+                          ('.nrrd',len(nrrdFiles)),
+                          ('.nhdr',len(nhdrFiles)),
+                          ('.hdr',len(hdrFiles)),
+                          ('.mhd',len(mhdFiles)),
+                          ('.mha',len(mhaFiles)),                               
+                          ]
+
+            self._helper.debug("Found "+str(sorted(listOfFiles, key=itemgetter(1), reverse=True)[0][1])+" files of type "+str(sorted(listOfFiles, key=itemgetter(1), reverse=True)[0][0]))
+
+            for nrrdFile in nrrdFiles:
+                caseFile = os.path.basename(nrrdFile)
+                if os.path.isfile(os.path.join(self._segDirButton.GetWidget().GetFileName(),caseFile)):
+                    # file exists in originals and segmentations directory, so we can add it to our list of cases
+                    self._defaultCaseEntry.GetWidget().AddValue(caseFile)
+                    self._defaultCaseEntry.GetWidget().SetValue(caseFile)
+
+    def GenerateAtlas(self):
+        # call the logic
+        labels = self._logic.GenerateAtlas(self._origDirButton.GetWidget().GetFileName(),
+                                                 self._segDirButton.GetWidget().GetFileName(),
+                                                 self._outDirButton.GetWidget().GetFileName(),
+                                                 self._defaultCaseEntry.GetWidget().GetValue(),
+                                                 self._regTypeRadios.GetWidget().GetWidget(0).GetSelectedState(),
+                                                 self._saveTransformCheckBox.GetWidget().GetSelectedState(),
+                                                 self._saveDeformationFieldCheckBox.GetWidget().GetSelectedState())
+
+        if labels:
+
+            for i in range(0,len(labels)): 
+                # loading atlas for label i
+                newVolNode = slicer.VolumesGUI.GetLogic().AddArchetypeScalarVolume(os.path.normpath(self._outDirButton.GetWidget().GetFileName()+"/atlas"+str(labels[i])+".nrrd"),'AtlasForLabel'+str(labels[i]))
+                displayNode = newVolNode.GetDisplayNode()
+                if displayNode:
+                    newDisplayNode = displayNode.NewInstance()
+                    newDisplayNode.Copy(displayNode)
+                    slicer.MRMLScene.AddNodeNoNotify(newDisplayNode)
+                    newVolNode.SetAndObserveDisplayNodeID(newDisplayNode.GetID())
+                    newDisplayNode.AutoWindowLevelOff()
+                    newDisplayNode.AutoWindowLevelOn()
+    
+            # now loading the atlas for all labels
+            newVolNode = slicer.VolumesGUI.GetLogic().AddArchetypeScalarVolume(os.path.normpath(self._outDirButton.GetWidget().GetFileName()+"/atlas.nrrd"),'AtlasAllLabels')
+
+            displayNode = newVolNode.GetDisplayNode()
+            if displayNode:
+                newDisplayNode = displayNode.NewInstance()
+                newDisplayNode.Copy(displayNode)
+                slicer.MRMLScene.AddNodeNoNotify(newDisplayNode)
+                newVolNode.SetAndObserveDisplayNodeID(newDisplayNode.GetID())
+                newDisplayNode.AutoWindowLevelOff()
+                newDisplayNode.AutoWindowLevelOn()
+
+
+            selectionNode = slicer.ApplicationLogic.GetSelectionNode()
+            selectionNode.SetReferenceActiveVolumeID(newVolNode.GetID())
+            slicer.ApplicationLogic.PropagateVolumeSelection()
+
+
+                  
+    def CreateOutVolumeNodes(self):
+
+        if not self._outVolumeSelector.GetSelected():
+
+            self._outVolumeSelector.SetSelectedNew("vtkMRMLScalarVolumeNode")
+            self._outVolumeSelector.ProcessNewNodeCommand("vtkMRMLScalarVolumeNode", "AtlasOutput")
+
+
+    def UpdateMRML(self):
+
+        if not self._updating:
+
+            self._updating = 1
+
+            self._updating = 0
+
+
+    def UpdateGUI(self):
+
+        if not self._updating:
+
+            self._updating = 1
+
+            self._updating = 0
+
+    def ProcessMRMLEvents(self,caller,event):
+
+        if caller == self.GetLogic().GetMRMLScene() and event == vtkMRMLScene_CloseEvent:
+            self.OnSceneClose()
+
+        elif caller == self.GetScriptedModuleNode():
+            self.UpdateGUI()
+
+
+    def BuildGUI(self):
+
+        self.GetUIPanel().AddPage("AtlasCreator","AtlasCreator","")
+        self._atlascreatorPage = self.GetUIPanel().GetPageWidget("AtlasCreator")
+        helpText = "**A simple Atlas Creator**, developed by Daniel Haehn."
+        aboutText = "This work is supported by NA-MIC, NAC, BIRN, NCIGT, and the Slicer Community. See http://www.slicer.org for details."
+        self._helpAboutFrame = self.BuildHelpAndAboutFrame(self._atlascreatorPage,helpText,aboutText)
+
+
+        self._moduleFrame.SetParent(self._atlascreatorPage)
+        self._moduleFrame.Create()
+        self._moduleFrame.SetLabelText("AtlasCreator")
+        self._moduleFrame.ExpandFrame()
+        slicer.TkCall("pack %s -side top -anchor nw -fill x -padx 2 -pady 2 -in %s" % (self._moduleFrame.GetWidgetName(),self._atlascreatorPage.GetWidgetName()))
+
+        self._topFrame.SetParent(self._moduleFrame.GetFrame())
+        self._topFrame.Create()
+        self._topFrame.SetLabelText("Input/Output")
+        self._topFrame.ExpandFrame()
+        slicer.TkCall("pack %s -side top -anchor nw -fill x -padx 2 -pady 2" % self._topFrame.GetWidgetName())
+
+        self._origDirButton.SetParent(self._topFrame.GetFrame())
+        self._origDirButton.Create()
+        self._origDirButton.GetWidget().SetText("Click to pick a directory")
+        self._origDirButton.SetLabelText("Original images:")
+        self._origDirButton.GetWidget().GetLoadSaveDialog().ChooseDirectoryOn()
+        slicer.TkCall("pack %s -side top -anchor nw -fill x -padx 2 -pady 2" % self._origDirButton.GetWidgetName())
+
+        self._origDirButton.GetWidget().GetLoadSaveDialog().Create()
+
+        self._segDirButton.SetParent(self._topFrame.GetFrame())
+        self._segDirButton.Create()
+        self._segDirButton.GetWidget().SetText("Click to pick a directory")
+        self._segDirButton.SetLabelText("Manual segmentations:")
+        self._segDirButton.GetWidget().GetLoadSaveDialog().ChooseDirectoryOn()
+        slicer.TkCall("pack %s -side top -anchor nw -fill x -padx 2 -pady 2" % self._segDirButton.GetWidgetName())
+
+        self._segDirButton.GetWidget().GetLoadSaveDialog().Create()
+
+        self._outDirButton.SetParent(self._topFrame.GetFrame())
+        self._outDirButton.Create()
+        self._outDirButton.GetWidget().SetText("Click to pick a directory")
+        self._outDirButton.SetLabelText("Output directory:")
+        self._outDirButton.GetWidget().GetLoadSaveDialog().ChooseDirectoryOn()
+        slicer.TkCall("pack %s -side top -anchor nw -fill x -padx 2 -pady 2" % self._outDirButton.GetWidgetName())
+
+        self._secondFrame.SetParent(self._moduleFrame.GetFrame())
+        self._secondFrame.Create()
+        self._secondFrame.SetLabelText("Registration")
+        self._secondFrame.ExpandFrame()
+        slicer.TkCall("pack %s -side top -anchor nw -fill x -padx 2 -pady 2" % self._secondFrame.GetWidgetName())
+
+        self._defaultCaseEntry.SetParent(self._secondFrame.GetFrame())
+        self._defaultCaseEntry.Create()
+        self._defaultCaseEntry.GetWidget().ReadOnlyOn()
+        self._defaultCaseEntry.SetLabelText("Default case:")
+        self._defaultCaseEntry.SetBalloonHelpString("The filename of the default case used for registration.")
+        slicer.TkCall("pack %s -side top -anchor nw -fill x -padx 2 -pady 2" % self._defaultCaseEntry.GetWidgetName())
+
+        self._regTypeRadios.SetParent(self._secondFrame.GetFrame())
+        self._regTypeRadios.Create()
+        self._regTypeRadios.SetLabelText("Registration Type:")
+
+        affineRadio = self._regTypeRadios.GetWidget().AddWidget(0)
+        affineRadio.SetText("Affine")
+
+        nonRigidRadio = self._regTypeRadios.GetWidget().AddWidget(1)
+        nonRigidRadio.SetText("Non-Rigid")
+        slicer.TkCall("pack %s -side top -anchor nw -fill x -padx 2 -pady 2" % self._regTypeRadios.GetWidgetName())
+
+        self._regTypeRadios.GetWidget().GetWidget(0).SetSelectedState(1)
+
+        self._saveTransformCheckBox.SetParent(self._secondFrame.GetFrame())
+        self._saveTransformCheckBox.Create()
+        self._saveTransformCheckBox.SetLabelText("Save Transforms:")
+        self._saveTransformCheckBox.GetWidget().SetSelectedState(1)
+        slicer.TkCall("pack %s -side top -anchor nw -fill x -padx 2 -pady 2" % self._saveTransformCheckBox.GetWidgetName())
+
+        self._saveDeformationFieldCheckBox.SetParent(self._secondFrame.GetFrame())
+        self._saveDeformationFieldCheckBox.Create()
+        self._saveDeformationFieldCheckBox.SetLabelText("Save Deformation Fields:")
+        self._saveDeformationFieldCheckBox.GetWidget().SetSelectedState(1)
+        slicer.TkCall("pack %s -side top -anchor nw -fill x -padx 2 -pady 2" % self._saveDeformationFieldCheckBox.GetWidgetName())
+
+        self._generateButton.SetParent(self._moduleFrame.GetFrame())
+        self._generateButton.Create()
+        self._generateButton.SetEnabled(1)
+        self._generateButton.SetText("Generate Atlas!")
+        self._generateButton.SetBalloonHelpString("Click to generate the atlas")
+        slicer.TkCall("pack %s -side top -anchor e -padx 2 -pady 2" % self._generateButton.GetWidgetName())
+
+
+    def TearDownGUI(self):
+        if self.GetUIPanel().GetUserInterfaceManager():
+            self.GetUIPanel().RemovePage("AtlasCreator")
+
+    def GetHelper(self):
+        return self._helper
+
+    def GetMyLogic(self):
+        return self._logic
---
+++ Modules/AtlasCreator/AtlasCreatorLogic.py
@@ -0,0 +1,264 @@
+from Slicer import slicer
+import os, glob
+
+class AtlasCreatorLogic(object):
+
+    def __init__(self,parentClass):
+
+        self._parentClass = parentClass
+
+    def GenerateAtlas(self,inputOriginalsPath,inputManualSegmentationsPath,outputPath,defCase,onlyAffineReg,saveTransforms,saveDeformationFields):
+        self._parentClass.GetHelper().debug("--------------------------------------------------------------------------------")
+        self._parentClass.GetHelper().debug("--------------------------------------------------------------------------------")
+        self._parentClass.GetHelper().debug("AtlasCreator: GenerateAtlas() called")
+        self._parentClass.GetHelper().debug("Path to Original Images: " + str(inputOriginalsPath))
+        self._parentClass.GetHelper().debug("Path to Manual Segmentations: " + str(inputManualSegmentationsPath))
+        self._parentClass.GetHelper().debug("Output path: " + str(outputPath))
+        self._parentClass.GetHelper().debug("Default case: " + str(defCase))
+        self._parentClass.GetHelper().debug("Use only affine registration: " + str(onlyAffineReg))
+        self._parentClass.GetHelper().debug("Save Transforms: " + str(saveTransforms))
+        self._parentClass.GetHelper().debug("Save Deformation Fields: " + str(saveDeformationFields))
+        self._parentClass.GetHelper().debug("--------------------------------------------------------------------------------")
+
+        if not inputOriginalsPath or not inputManualSegmentationsPath or not outputPath:
+            self._parentClass.GetHelper().debug("Input paths for the original images and the manual segmentations are required.")
+            return 0
+
+        # find default case
+        if defCase:
+            defaultCase = os.path.join(inputOriginalsPath, defCase)
+            defaultCaseSeg = os.path.join(inputManualSegmentationsPath, defCase)
+
+        # get the extension of the default case, we will use to find all other cases
+        extension = os.path.splitext(defCase)[1]
+
+        # get the label values of the default case
+        reader = slicer.vtkNRRDReader()
+        reader.SetFileName(defaultCaseSeg)
+        reader.Update()
+        defCaseImage = slicer.vtkImageData()
+        defCaseImage.DeepCopy(reader.GetOutput())
+        labels = self.GetLabels(defCaseImage)
+        self._parentClass.GetHelper().debug("LabelMap values found in default case: "+str(labels))
+
+        # get the Slicer paths without environment variables
+        slicerDir = os.path.normpath(str(slicer.Application.GetBinDir()))+"/../"
+        slicerLauncher = os.path.normpath(slicerDir+"/Slicer3")
+        slicerBinDir = os.path.normpath(str(slicer.Application.GetBinDir()))+"/"
+        slicerPluginsDir = os.path.normpath(slicerDir+"lib/Slicer3/Plugins")+"/"
+        slicerTempDir = os.path.normpath(str(slicer.Application.GetTemporaryDirectory()))
+
+        #
+        # registration stage
+        #
+        self._parentClass.GetHelper().debug("*** (1) Registration Stage ***")
+
+        for origFile in glob.glob(os.path.join(inputOriginalsPath, '*'+extension)):
+
+            if origFile!=defaultCase:
+                self._parentClass.GetHelper().debug("Registering "+origFile+" to default case..")
+
+                caseFile = os.path.basename(origFile)
+                caseName = caseFile.rstrip('.nrrd')
+                if saveTransforms:
+                    outputTransform = outputPath+"/"+caseName+".mat"
+                else:
+                    outputTransform = slicerTempDir+"/"+caseName+".mat"
+
+                os.system(slicerLauncher+" --launch "+slicerPluginsDir+self.Register(defaultCase,origFile,outputTransform,onlyAffineReg))
+
+
+        #
+        # resample stage
+        #
+        self._parentClass.GetHelper().debug("*** (2) Resample Stage ***")
+
+        for segmentfile in glob.glob(os.path.join(inputManualSegmentationsPath, '*'+extension)):
+
+            if segmentfile!=defaultCaseSeg:
+                self._parentClass.GetHelper().debug("Resampling "+segmentfile+" using the calculated transform..")
+
+                caseFile = os.path.basename(segmentfile)
+                caseName = caseFile.rstrip('.nrrd')
+                origFile = inputOriginalsPath+"/"+caseFile
+                if saveTransforms:
+                    inputTransform = outputPath+"/"+caseName+".mat"
+                else:
+                    inputTransform = slicerTempDir+"/"+caseName+".mat"
+                outputRegisteredSegmentation = slicerTempDir+"/"+caseName+".nrrd"
+
+                os.system(slicerLauncher+" --launch "+slicerPluginsDir+self.Resample(segmentfile,origFile,inputTransform,outputRegisteredSegmentation))
+
+
+        #
+        # save deformationfield stage
+        #
+        self._parentClass.GetHelper().debug("*** (3) Save Deformationfields ***")
+
+        if not saveDeformationFields or onlyAffineReg:
+            self._parentClass.GetHelper().debug("Saving was disabled or only affine registration was selected.")
+        else:
+            for origFile in glob.glob(os.path.join(inputOriginalsPath, '*'+extension)):
+
+                if origFile!=defaultCase:
+                    self._parentClass.GetHelper().debug("Saving the deformation field for "+origFile+"..")
+
+                    caseFile = os.path.basename(origFile)
+                    caseName = caseFile.rstrip('.nrrd')
+                    if saveTransforms:
+                        inputTransform = outputPath+"/"+caseName+".mat"
+                    else:
+                        inputTransform = slicerTempDir+"/"+caseName+".mat"
+                    outputDeformationField = outputPath+"/"+caseName+"DeformationField.nrrd"
+
+                    os.system(slicerLauncher+" --launch "+slicerPluginsDir+self.SaveDeformationField(origFile,inputTransform,outputDeformationField))
+
+        #
+        # create atlas stage
+        #
+        self._parentClass.GetHelper().debug("*** (4) Create Atlas Stage ***")
+
+        atlas = slicer.vtkImageData()
+        imageDataBuffer = slicer.vtkImageData()
+        firstAtlas = True
+
+        for label in labels:
+            
+            # for each label, we create an atlas using all manual segmentations
+            currentLabelAtlas = slicer.vtkImageData()
+            firstrun = True        
+
+            for segmentfile in glob.glob(os.path.join(inputManualSegmentationsPath, '*'+extension)):
+                
+                # read the manual segmentation
+                reader = slicer.vtkNRRDReader()
+                reader.SetFileName(segmentfile)
+                reader.Update()
+
+                currentSegmentation = slicer.vtkImageData()
+                currentSegmentation.DeepCopy(reader.GetOutput())
+                
+                # threshold for current label
+                threshold = slicer.vtkImageThreshold()
+                threshold.SetInput(currentSegmentation)
+                threshold.ThresholdBetween(label,label)
+                threshold.ReplaceInOn()
+                threshold.ReplaceOutOn()
+                threshold.SetInValue(1)
+                threshold.SetOutValue(0)
+                threshold.Update()
+
+                if firstrun:
+                    # copy just the first segmentation
+                    currentLabelAtlas.DeepCopy(threshold.GetOutput())
+                    firstrun = False
+                else:
+                    # copy the combined segmentation
+                    add = slicer.vtkImageMathematics()
+                    add.SetInput1(threshold.GetOutput())
+                    add.SetInput2(currentLabelAtlas)
+                    add.SetOperationToAdd()
+                    add.Update()
+                    currentLabelAtlas.DeepCopy(add.GetOutput())
+    
+             #numberOfManualSegmentations = len(glob.glob(os.path.join(inputManualSegmentationsPath, '*.nrrd')))
+    
+#            # now we divide our label atlas by the number of manual segmentations
+#            divide = slicer.vtkImageMathematics()
+#            divide.SetInput(currentLabelAtlas)
+#            divide.SetOperationToMultiplyByK()
+#            divide.SetConstantK(1/numberOfManualSegmentations)
+#            divide.Update()
+#            
+    
+            # and combine it with the other label atlases
+            if firstAtlas:
+                atlas.DeepCopy(currentLabelAtlas)
+                firstAtlas = False
+            else:
+                add = slicer.vtkImageMathematics()
+                add.SetInput1(currentLabelAtlas)
+                add.SetInput2(atlas)
+                add.SetOperationToAdd()
+                add.Update()    
+                atlas.DeepCopy(add.GetOutput())
+
+            writer = slicer.vtkNRRDWriter()
+            writer.SetFileName(os.path.normpath(outputPath+"/atlas"+str(label)+".nrrd"))
+            writer.SetInput(currentLabelAtlas)
+            writer.Update()
+
+        writer = slicer.vtkNRRDWriter()
+        writer.SetFileName(os.path.normpath(outputPath+"/atlas.nrrd"))
+        writer.SetInput(atlas)
+        writer.Update()
+            
+        #
+        # done creating the atlas
+        #
+        self._parentClass.GetHelper().debug("Atlas was created and saved to "+os.path.normpath(outputPath+"/atlas.nrrd")+".")
+        self._parentClass.GetHelper().debug("--------------------------------------------------------------------------------")
+        self._parentClass.GetHelper().debug("--------------------------------------------------------------------------------")
+        self._parentClass.GetHelper().debug("--------------------------------------------------------------------------------")
+        
+        return labels
+
+    def GetLabels(self,labelMap):
+        accum = slicer.vtkImageAccumulate()
+        accum.SetInput(labelMap)
+        accum.UpdateWholeExtent()
+        accum.Update()
+        data = accum.GetOutput()
+        numBins = accum.GetComponentExtent()[1]
+        nonZeroLabels = []
+        for i in range(1, numBins + 1):
+            numVoxels = data.GetScalarComponentAsDouble(i,0,0,0)
+            if (numVoxels>0):
+                nonZeroLabels.append(i)
+        return nonZeroLabels
+
+    def Register(self,defaultCase,origFile,outputTransform,onlyAffineReg):
+        registrationCommand = "BRAINSFit"
+        registrationCommand += " --fixedVolume "+os.path.normpath(defaultCase)
+        registrationCommand += " --movingVolume "+os.path.normpath(origFile)
+        registrationCommand += " --outputTransform "+os.path.normpath(outputTransform)
+        registrationCommand += " --maxBSplineDisplacement 10.0 --outputVolumePixelType short --backgroundFillValue 0.0 --interpolationMode Linear"
+        #registrationCommand += " --maskProcessingMode  ROIAUTO --ROIAutoDilateSize 3.0 --maskInferiorCutOffFromCenter 65.0"
+        registrationCommand += " --useRigid --useScaleVersor3D --useScaleSkewVersor3D"
+        #registrationCommand += " --initializeTransformMode useCenterOfHeadAlign --useRigid --useScaleVersor3D --useScaleSkewVersor3D"
+        registrationCommand += " --useAffine"
+
+        if not onlyAffineReg:
+            registrationCommand += " --useBSpline"
+
+        registrationCommand += " --numberOfSamples 100000 --numberOfIterations 1500"
+        registrationCommand += " --translationScale 1000.0 --reproportionScale 1.0 --skewScale 1.0 --splineGridSize 28,20,24 --fixedVolumeTimeIndex 0"
+        registrationCommand += " --movingVolumeTimeIndex 0 --medianFilterSize 0,0,0 --numberOfHistogramBins 50 --numberOfMatchPoints 10 --useCachingOfBSplineWeightsMode ON"
+        registrationCommand += " --useExplicitPDFDerivativesMode AUTO --relaxationFactor 0.5 --failureExitCode -1 --debugNumberOfThreads -1"
+        registrationCommand += " --debugLevel 0 --costFunctionConvergenceFactor 1e+9 --projectedGradientTolerance 1e-5"
+
+
+        return registrationCommand
+
+
+    def Resample(self,segmentFile,origFile,inputTransform,outputRegisteredSegmentation):
+        resampleCommand = "BRAINSResample"
+        resampleCommand += " --inputVolume "+os.path.normpath(segmentFile)
+        resampleCommand += " --referenceVolume "+os.path.normpath(origFile)
+        resampleCommand += " --warpTransform "+os.path.normpath(inputTransform)
+        resampleCommand += " --outputVolume "+os.path.normpath(outputRegisteredSegmentation)
+        resampleCommand += " --defaultValue 8.0 --pixelType short --interpolationMode NearestNeighbor"
+
+        return resampleCommand
+
+
+    def SaveDeformationField(self,origFile,inputTransform,outputDeformationField):
+    
+        saveDefFieldCommand = "BSplineToDeformationField"
+        saveDefFieldCommand += " --refImage "+os.path.normpath(origFile)
+        saveDefFieldCommand += " --tfm "+os.path.normpath(inputTransform)
+        saveDefFieldCommand += " --defImage "+os.path.normpath(outputDeformationField)
+
+        return saveDefFieldCommand
+
+
