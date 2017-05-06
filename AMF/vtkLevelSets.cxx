50f7c8a8012a72dff518c830377aa4832717cd5d
jcfr <jcfr@3bd1e089-480b-0410-8dfb-8563597acbee>
2017-05-06 04:48:04 +0000

    COMP: vtkLevelSets: Removed variable to fix unused-but-set-variable warning
    git-svn-id: http://svn.slicer.org/Slicer3/branches/Slicer4-EMSegment@17129 3bd1e089-480b-0410-8dfb-8563597acbee

--- AMF/vtkLevelSets.cxx
+++ AMF/vtkLevelSets.cxx
@@ -1259,7 +1259,7 @@ unsigned char vtkLevelSets::CheckConvergence( )
      int   p;;
      int   cnt1,cnt2,total;
      float converged_check1;
-     float converged_check2;
+     // float converged_check2;
      float converged_check;
 
    if (GB_debug) fprintf(stderr, "vtkLevelSets::CheckConvergence( ) begin \n");
@@ -1295,10 +1295,10 @@ unsigned char vtkLevelSets::CheckConvergence( )
    }
  
    converged_check1 = ((float)cnt2)/((float)total);
-   converged_check2 = ((float)cnt1)/((float)total);
+   //converged_check2 = ((float)cnt1)/((float)total);
     
    vtkDebugMacro( << cnt1 << "," << cnt2 << "," << 
-         total << "," << converged_check1 << "," << converged_check2);
+         total << "," << converged_check1 );
        
    // in simulation experiments, care about shrinking also, so use
    //   a different convergence check   
